78aa86597ba5bf42710b003b39b343b9e96ee664
Cheung, Wilson <wilson.cheung@cengage.com>
2016-05-23 22:22:42 -0700

    a

--- client/components/Header/component.jsx
+++ client/components/Header/component.jsx
@@ -6,7 +6,12 @@ import './style.css';
 export default class Header extends React.Component {
 
     render() {
-        const value = _.take([1, 2, 3])[0];
-        return (<span className="header">Component {value}  - content</span>);
+        const value = _.take([2, 3, 1])[0];
+        return (
+            <div>
+                <span className="header">Component {value} - content</span>
+                <li>Component 2a - content</li>
+            </div>
+        );
     }
 }
